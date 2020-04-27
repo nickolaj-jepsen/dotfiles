a4b97044f233e139e515633cd39434b12fa312fa
Nickolaj Jepsen <nij@billigvvs.dk>
2020-04-27 10:07:04 +0200

    Update monitor setup

--- bspwm/.config/init/config/polybar/arch-desktop.sh
+++ bspwm/.config/init/config/polybar/arch-desktop.sh
@@ -1,3 +1,3 @@
-MONITOR=DP-4 polybar -c ~/.config/polybar/main.ini -r top &
-MONITOR=DP-0 polybar -c ~/.config/polybar/external-left.ini -r external-left &
-MONITOR=DP-3 polybar -c ~/.config/polybar/external-right.ini -r external-right &
+MONITOR=DP-0 polybar -c ~/.config/polybar/main.ini -r top &
+MONITOR=DP-2 polybar -c ~/.config/polybar/external-left.ini -r external-left &
+MONITOR=DP-5 polybar -c ~/.config/polybar/external-right.ini -r external-right &
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,6 +4,7 @@ set PATH ~/.config/panel $PATH
 set PATH ~/bin $PATH
 set PATH ~/Development/devenv/bin $PATH
 set PATH /snap/bin $PATH
+set PATH /var/lib/snapd/snap/bin $PATH
 set PATH /usr/bin/core_perl/ $PATH
 set PATH ~/.local/bin $PATH
 set PATH ~/.dotnet/tools $PATH
