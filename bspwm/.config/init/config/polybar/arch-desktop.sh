71b127206c790031b9089d101b7e19bb046e1d60
Nickolaj Jepsen <nij@billigvvs.dk>
2020-08-19 07:46:27 +0200

    Updated arch-home bspwm init

--- bspwm/.config/init/config/polybar/arch-desktop.sh
+++ bspwm/.config/init/config/polybar/arch-desktop.sh
@@ -1,3 +1,3 @@
 MONITOR=DP-0 polybar -c ~/.config/polybar/main.ini -r top &
-MONITOR=DP-2 polybar -c ~/.config/polybar/external-left.ini -r external-left &
+MONITOR=HDMI-0 polybar -c ~/.config/polybar/external-left.ini -r external-left &
 MONITOR=DP-5 polybar -c ~/.config/polybar/external-right.ini -r external-right &
