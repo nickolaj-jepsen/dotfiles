ff362fb4ca8c71280dcfb6a844f454d84399daf6
Nickolaj Jepsen <nij@billigvvs.dk>
2020-11-11 11:21:03 +0100

    Replace fish z with to

--- fish/.config/fish/conf.d/30_alias.fish
+++ fish/.config/fish/conf.d/30_alias.fish
@@ -7,3 +7,14 @@ if type -q devenv
     abbr shop "devenv"
     abbr flex "devenv --flex"
 end
+
+if type -q snap
+    set PYCHARM_SNAP (snap list | grep 'pycharm' | awk '{ print $1 }')
+    if test -n "$PYCHARM_SNAP"
+        alias pycharm "snap run $PYCHARM_SNAP"
+    end
+end
+
+if type -q to
+    abbr z "to"
+end
