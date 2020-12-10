49642f56dcc2d844076ae0d150a372bf1301facc
Nickolaj Jepsen <nij@billigvvs.dk>
2020-12-10 15:46:04 +0100

    Add devenv aliases

--- fish/.config/fish/conf.d/30_alias.fish
+++ fish/.config/fish/conf.d/30_alias.fish
@@ -6,6 +6,19 @@ if type -q devenv
     abbr de "devenv"
     abbr shop "devenv"
     abbr flex "devenv --flex"
+    abbr all "devenv -s all"
+    function shell --argument-names 'shop'
+        if set -q $argv[1]
+            set shop billigvvs.dk
+        end
+        devenv -s $shop django manage shell_plus
+    end
+    function logs --argument-names 'shop'
+        if set -q $argv[1]
+            set shop billigvvs.dk
+        end
+        devenv -s $shop django logs
+    end
 end
 
 if type -q snap
