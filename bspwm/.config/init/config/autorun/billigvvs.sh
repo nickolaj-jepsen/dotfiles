86690d5b098548cc2e6cb25645adb8df3a856030
Nickolaj Jepsen <nij@billigvvs.dk>
2023-05-25 10:50:26 +0200

    fix: update work autorun

--- bspwm/.config/init/config/autorun/billigvvs.sh
+++ bspwm/.config/init/config/autorun/billigvvs.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 chromium &
-snap run pycharm-professional &
 smerge &
-snap run teams &
-snap run slack &
-kitty --class terminal-mon3-2 fish -C "ssh home -D 9999" &
-kitty --class terminal-mon3-2 fish -C "pycharm" &
+teams &
+slack &
+/home/nickolaj/.local/bin/pycharm &
