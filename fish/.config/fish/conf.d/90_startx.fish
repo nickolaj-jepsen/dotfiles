d02e90ceabf28f2fabf19ec99437949b0cad9aaf
Nickolaj Jepsen <nij@billigvvs.dk>
2024-08-28 20:33:32 +0200

    chore: update

--- fish/.config/fish/conf.d/90_startx.fish
+++ fish/.config/fish/conf.d/90_startx.fish
@@ -1,6 +1,7 @@
 # start X at login
-if status --is-login
-    if test -z "$DISPLAY" -a $XDG_VTNR -eq 1
-        exec startx -- -keeptty
-    end
-end
+#if status --is-login
+#    if test -z "$DISPLAY" -a $XDG_VTNR -eq 1
+#        Hyprland
+#        exec startx -- -keeptty
+#    end
+#end
