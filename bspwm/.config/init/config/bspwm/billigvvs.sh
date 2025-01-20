12c74cc8cbbea644d9466f5780a2845fd727fb74
Nickolaj Jepsen <nij@billigvvs.dk>
2025-01-20 12:31:57 +0100

    chore: update files from work pc

--- bspwm/.config/init/config/bspwm/billigvvs.sh
+++ bspwm/.config/init/config/bspwm/billigvvs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-xrandr --output HDMI-2 --mode 1920x1080 --pos 0x336 --rotate normal --output HDMI-1 --primary --mode 1920x1200 --pos 1920x288 --rotate normal --output VIRTUAL-1 --off --output VGA-1 --mode 1920x1200 --pos 3840x0 --rotate left
+xrandr --output HDMI-1 --mode 1920x1080 --pos 0x336 --rotate normal --output HDMI-2 --primary --mode 1920x1200 --pos 1920x288 --rotate normal --output VIRTUAL-1 --off --output VGA-1 --mode 1920x1200 --pos 3840x0 --rotate left
 
-bspc monitor HDMI-1 -d web code term music chat
-bspc monitor HDMI-2 -d mon2-1 mon2-2
+bspc monitor HDMI-2 -d web code term music chat
+bspc monitor HDMI-1 -d mon2-1 mon2-2
 bspc monitor VGA-1 -d mon3-1 mon3-2
--- bspwm/.config/init/config/polybar/billigvvs.sh
+++ bspwm/.config/init/config/polybar/billigvvs.sh
@@ -1,3 +1,3 @@
-MONITOR=HDMI-1 polybar -c ~/.config/polybar/main.ini -r top &
-MONITOR=HDMI-2 polybar -c ~/.config/polybar/external-left.ini -r external-left &
+MONITOR=HDMI-2 polybar -c ~/.config/polybar/main.ini -r top &
+MONITOR=HDMI-1 polybar -c ~/.config/polybar/external-left.ini -r external-left &
 MONITOR=VGA-1 polybar -c ~/.config/polybar/external-right.ini -r external-right &
