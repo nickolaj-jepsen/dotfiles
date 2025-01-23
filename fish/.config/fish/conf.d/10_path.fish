8c4db01d3ca42ac659c91f09de9672a99f17010a
Nickolaj Jepsen <nij@billigvvs.dk>
2025-01-23 21:02:22 +0100

    fix(fish): use fish_add_path

--- fish/.config/fish/conf.d/10_path.fish
+++ fish/.config/fish/conf.d/10_path.fish
@@ -1,20 +1,15 @@
-function append_path
-    if test -d $argv
-        set PATH $argv $PATH
-    end
-end
-
-append_path ~/.config/panel
-append_path ~/bin
-append_path ~/Development/devenv/bin  # billigvvs home development env
-append_path /home/nij/src/devenv/bin  # billigvvs dev server development env
-append_path /var/lib/snapd/snap/bin
-append_path /usr/bin/core_perl/
-append_path ~/.local/bin
-append_path ~/.yarn/bin
-append_path ~/.krew/bin
-append_path /snap/bin
-append_path ~/.garden/bin
-append_path ~/.cargo/bin
-append_path ~/.cargo/env 
-append_path ~/.rye/shims 
+fish_add_path ~/.config/panel
+fish_add_path ~/bin
+fish_add_path ~/Development/devenv/bin  # billigvvs home development env
+fish_add_path /home/nij/src/devenv/bin  # billigvvs dev server development env
+fish_add_path /var/lib/snapd/snap/bin
+fish_add_path /usr/bin/core_perl/
+fish_add_path ~/.local/bin
+fish_add_path ~/.yarn/bin
+fish_add_path ~/.krew/bin
+fish_add_path /snap/bin
+fish_add_path ~/.garden/bin
+fish_add_path ~/.cargo/bin
+fish_add_path ~/.cargo/env 
+fish_add_path ~/.rye/shims 
+fish_add_path ~/.bun/bin
